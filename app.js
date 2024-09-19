19886df4eb1e7a4d6697cbb5dd87f47434b81ee0
s2yedzohaib <s2yedzohaib@gmail.com>
2024-09-19 11:16:55 -0700

    Complete

---
+++ app.js
@@ -0,0 +1,295 @@
+// HOME ASSIGNMENT:
+
+
+// Chapter 10 (if statements)
+
+// Q1:
+// var city = "karachi" ;
+// if ( city = "karachi" ) {
+//     console.log("The City Of Lights");
+// }
+
+// Q2:
+// var x = 2;
+// var y = 2;
+// if ( x === y ) {
+//     var z = +prompt("Enter value of z");
+//     document.write(z)
+// }
+
+// Q3:
+// var zipCode = +prompt("Enter your city zip code!");
+// if ( zipCode === 10010 ) {
+//     alert("Karachi");
+// }
+// else {
+//     alert("Please write correct city!")
+// }
+
+// Q4:
+
+
+
+// Chapter 11 (Comparison Operators)
+
+// Q1:
+// var x = 3;
+// var y = 4;
+// if ( x != y ) {
+//     document.write("x & y are not equal!");
+// }
+
+// Q2:
+// var x = 8;
+// var y = 5;
+// if ( x >= y ) {
+//     document.write("Yes x is greater or equal to y.");   
+// }
+
+// Q3:
+// var x = 5;
+// if ( x != 4 ) {
+//     x = 4;
+//     document.write("Yes x is not equal to 4.");
+// }
+
+// Q4:
+// var x = +prompt("Enter value of x.");
+// if ( x = 4 ) {
+//     alert("Congrats! You were right!");
+// }
+
+// Q5:
+// var name = prompt("Enter your name:");
+// if ( name = "ahmed" ) {
+//     alert("No match!");
+// }
+
+
+
+// Chapter 12 (if…else and else if statements)
+
+// Q1:
+// var x = +prompt("Enter value of x:");
+// var y = +prompt("Enter value of y:");
+// if ( x >= y ) {
+//     alert("x is greater or equal than y.");
+// }
+// else {
+//     alert("x is not greater or equal to y.")
+// }
+
+// Q2:
+// var totalMarks = +prompt("Enter total marks:");
+// var obtainedMarks = +prompt("Enter obtained marks:");
+// var percentage = obtainedMarks * 100 / totalMarks;
+// if (percentage <= 30) {
+//     console.log("FAIL!");
+// }
+// else if (percentage <= 50) {
+//     console.log("Your grade is D");
+// }
+// else if (percentage <= 60) {
+//     console.log("Your grade is C");
+// }
+// else if (percentage <= 70) {
+//     console.log("Your grade is B");
+// }
+// else if (percentage <= 79) {
+//     console.log("Your grade is A");
+// }
+// else if (percentage <= 100) {
+//     console.log("Your grade is A+");
+// }
+// else {
+//     console.log("invalid percentage!")
+// }
+
+// Q3:
+// var a = +prompt("Enter value of a:");
+// if (a === 10) {
+//     alert("a is 10");
+// }
+// else {
+//     alert(`The correct value of a is ${a}`);
+// }
+
+// Q4:
+// var city = prompt("Enter your city:");
+// if (true) {
+//     alert(`its ${city}`);
+// }
+
+
+// ----------------------------------------------------------
+
+
+// CLASS ASSIGNMENT:
+
+
+// USER INPUT & CONDITIONAL STATEMENT
+
+// Q1:
+// var city = prompt("Enter your city name:");
+// if ( city = "karachi" ) {
+//     alert("Welcome to city of lights!");
+// }
+
+// Q2:
+// var gender = prompt("Enter your gender:");
+// if ( gender = "male" )  {
+//     alert("Good Morninig Sir!");
+// }
+// else if ( gender = "female" ) {
+//     alert("Good Morning Ma'am!");
+// }
+
+// Q3:
+// var signal = prompt("Enter Signal Color:");
+// if ( signal = "red" ) {
+//     alert("Must Stop!");
+// }
+// else if ( signal = "yellow" ) {
+//     alert("Ready to move!");
+// }
+// else if ( signal = "green" ) {
+//     alert("Move now!");
+// }
+// else {
+//     alert("Please enter correct color!");
+// }
+
+// Q4:
+// var fuel = prompt("Enter remaining fuel in your car:");
+// if (fuel <= 0.25) {
+//     alert("Please refill the fuel in your car!");
+// }
+
+// Q5:
+// a:
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// Result: true
+
+// b:
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// Result: false
+
+// c:
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// Result: Condition1: false
+//         Condition2: true
+//         Condition3: false
+//         Condition4: true
+
+// d:
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// Result: true
+
+// e:
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// f:
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+
+// Q6:
+// var totalMarks = 300;
+// var sub1 = +prompt("Enter English Marks:");
+// var sub2 = +prompt("Enter Urdu Marks:");
+// var sub3 = +prompt("Enter Sindhi Marks:");
+// var obtainedMarks = sub1 + sub2 + sub3;
+// var percentage = (obtainedMarks * 100) / totalMarks;
+// var grade , remarks;
+// if (percentage >= 80) {
+//     grade = "A1";
+//     remarks = "Excellent";
+// }
+// else if (percentage >= 70) {
+//     grade = "A";
+//     remarks = "Good";
+// }
+// else if (percentage >= 60) {
+//     grade = B
+//     remarks = "You need to improve";
+// }
+// else if (percentage < 60) {
+//     grade = "Fail";
+//     remarks = "Sorry";
+// }
+// document.write(`Total marks :  ${totalMarks}`);
+// document.write(`Marks Obtained :  ${obtainedMarks}`);
+// document.write(`Percentage :  ${percentage} %`);
+// document.write(`Grade :  ${grade}`);
+// document.write(`Remarks :  ${remarks}`);
+
+// Q7:
+// var secretNumber = 2;
+// var userNumber = prompt("Guess a number from 1 to 10");
+// if (userNumber == secretNumber) {
+//     alert("Bingo! Correct answer.");
+// }
+// else if (userNumber == 1 , userNumber == 3) {
+//     alert("Close enough to the correct answer.");
+// }
+// else {
+//     alert("Sorry! Wrong answer.");
+// }
+
+// Q8:
+
+// Q9:
+
+// Q10:
+
+// Q11:
+
+// Q12:
+
+
+
+// IF...ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS
+
+
+// Q1:
+
+// Q2:
+
+// Q3:
+
+// Q4:
+
+// Q5:
+
+// Q6:
+
+// Q7:
